1022448d4586d07299f917a37060a9d282243fcf
devopscoder331 <devopscoder31@gmail.com>
2020-07-20 22:55:55 +0300

    main add busybox httpd

--- main.tf
+++ main.tf
@@ -3,10 +3,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0e8286b71b81c3cc1"
+  ami = "ami-0d359437d1756caa8"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
-    Name = "terraform-example"
+    Name = "terraform-example-web"
   }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terrafom-example-group"
+
+    ingress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 }
