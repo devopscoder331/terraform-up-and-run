1a82bbc9496695cfbffdee430c443b6cbce71c88
my account <devopscoder31@gmail.com>
2020-07-23 21:21:20 +0300

    deply autoscaling group

--- main.tf
+++ main.tf
@@ -2,22 +2,36 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0d359437d1756caa8"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0d359437d1756caa8"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance2.id]
+  security_groups = [aws_security_group.instance2.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  tags = {
-    Name = "terraform-example-web"
-  }
+
   lifecycle {
     create_before_destroy = true
   }
+}
+
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_security_group" "instance2" {
@@ -35,4 +49,12 @@ variable "server_port" {
   description = "The port the server will use for http request"
   type = number
   default = 8080
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
