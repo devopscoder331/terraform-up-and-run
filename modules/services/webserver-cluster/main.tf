22eec0a406c041d616b55d7e0df1bda788cb83c8
devopscoder331 <devopscoder31@gmail.com>
2020-08-03 09:38:06 +0300

    Change aws_security_group inline block to separate resource

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -78,23 +78,28 @@ resource "aws_security_group" "instance2" {
 }
 
 resource "aws_security_group" "alb" {
-    name = "${var.cluster_name}-alb"
+  name = "${var.cluster_name}-alb"
 
-    # allow all input http request
-    ingress {
-      from_port = local.http_port
-      to_port = local.http_port
-      protocol = local.tcp_protocol
-      cidr_blocks = local.all_ips
-    }
+}
 
-    # allow all output request
-    egress {
-      from_port = local.any_port
-      to_port = local.any_port
-      protocol = local.any_protocol
-      cidr_blocks = local.all_ips
-    }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 data "aws_vpc" "default" {
